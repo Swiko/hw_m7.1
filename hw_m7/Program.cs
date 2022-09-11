234a45f857a23865e92aed84717d9ff38449cdb7
Никита Холодков <hmd.swiko@gmail.com>
2022-09-11 23:50:09 +0300

    New try v0.1

--- hw_m7/Program.cs
+++ hw_m7/Program.cs
@@ -11,66 +11,30 @@ namespace hw_m7
     {
         static void Main(string[] args)
         {
+
             string path = @"E:\directory";
-            int choise = 0;
-            int id = 0;
-            
-            int forArray = File.ReadAllLines(path).Length;
-            Directory[] AllEmployee = new Directory[forArray];
-            Directory newEmp = new Directory();
+            Worker worker = new Worker();
 
             while (true)
             {
-                Console.WriteLine("\n1: for create new employee.\n" +
-                                    "2: for print all employees.\n" +
-                                    "3: for print employee by id.\n" +
-                                    "0: for close program.\n");
 
-                choise = int.Parse(Console.ReadLine());
+                Console.Write("Menu: \n Input 0 - for close programm: " +
+                    "                \n Input 1 - for add new worker: " +
+                    "                \n Input 2 - for print repository: " +
+                    "                \n ");
 
-                if (choise == 0) // Закрытие программы
+                int choise = int.Parse(Console.ReadLine());
+                
+
+                if (choise == 0)
                 {
                     break;
-                }
-                // Создание записи
+                } 
                 else if (choise == 1)
                 {
-                    newEmp.NewEmployee(newEmp, path);
+                    worker.CreateWorker(worker, path);
                 }
-                // Вывод всех сотрудников
-                else if (choise == 2)
-                {
-                    newEmp.OutputInfo(AllEmployee, path);
-                }
-                // Вывод сотрудника по id и работа с ним
-                else if (choise == 3)
-                {
-                    Console.Write("Input searched id: ");
-                    id = int.Parse(Console.ReadLine());
-
-                    newEmp.OutputInfo(AllEmployee, path, id);
-                    Console.WriteLine("\n0: for delete this employee.\n" +
-                                      "1: for edit this employee.\n");
-                    choise = int.Parse(Console.ReadLine());
-                    switch (choise)
-                    {
-                        case 0:
-                            newEmp.DeleteEmployee(path, id);
-                            Console.WriteLine("\nEmployee Deleted");
-                            break;
-                        case 1:
-                            newEmp.EditEmployee(path, id);
-                            break;
-                    }
-
-                    newEmp.OutputInfo(AllEmployee, path, id);
-                }
-                // Сотрировка сотрудников по дате заведения
-                else if (choise == 4)
-                {
-                    
-                }
-            }            
+            }
         }
     }
 }
---
+++ hw_m7/Worker.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace hw_m7
+{
+    public struct Worker
+    {
+
+        public int workerId;
+        public DateTime recordCreationDate;
+        public string workerFullName;
+        public int workerAge;
+        public int workerGrowth;
+        public DateTime workerDateOfBirth;
+        public string workePlaceOfBirth;
+
+        // Конструктор Структуры
+        public Worker(int id, DateTime dateOfCreation, string fullName, int age, int growth, DateTime dateOfBirth, string placeOfBirth)
+        {
+            this.workerId = 0;
+            this.recordCreationDate = dateOfCreation;
+            this.workerFullName = fullName;
+            this.workerAge = age;
+            this.workerGrowth = growth;
+            this.workerDateOfBirth = dateOfBirth;
+            this.workePlaceOfBirth = placeOfBirth;
+        }
+
+        //Функция записи в файл
+        private string Print()
+        {
+            return $"{workerId}#{recordCreationDate}#{workerFullName}#{workerAge}#{workerGrowth}#{workerDateOfBirth}#{workePlaceOfBirth}\n";
+        }
+
+        //Генерация ID для нового сотрудника
+        private int NewWorkerId (string path)
+        {
+            
+            if (File.Exists(path))
+            {
+                return (File.ReadAllLines(path).Length) + 1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+
+        //Добавление нового сотрудника
+        public void CreateWorker(Worker worker, string path)
+        {
+
+            worker.workerId = NewWorkerId(path);
+            worker.recordCreationDate = DateTime.Now;
+            Console.Write("Full name: ");
+            worker.workerFullName = Console.ReadLine();
+            Console.Write("Age: ");
+            worker.workerAge = int.Parse(Console.ReadLine());
+            Console.Write("Growth: ");
+            worker.workerGrowth = int.Parse(Console.ReadLine());
+            Console.Write("Date of birth: ");
+            worker.workerDateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.Write("Place of birth: ");
+            worker.workePlaceOfBirth = Console.ReadLine();
+
+            File.AppendAllText(path, worker.Print());
+        }
+
+    }
+}
